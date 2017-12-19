462235844135e44a26a1b3b057a22556566531e5
Jacques Troussard <jacques.troussard@gmail.com>
2017-12-19 17:35:30 -0500

    chapter one project one page 24
    set listeners for each button
    anonymous inner class implementation not coded

--- GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.andriod.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
@@ -14,8 +15,22 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // True button
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do things
+            }
+        });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
-        
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do things
+            }
+        });
+
     }
 }

d7502d4f1e1468c21d20bc06b37f61d84f392388
Jacques Troussard <jacques.troussard@gmail.com>
2017-12-20 01:59:23 -0500

    chapter one project one page 32
    Challenge: Customizing the Toast - finished

--- GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.andriod.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,15 +22,20 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast t = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP,0,0);
+                t.show();
             }
         });
 
+        // False button
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast t = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP,0,0);
+                t.show();
             }
         });
 

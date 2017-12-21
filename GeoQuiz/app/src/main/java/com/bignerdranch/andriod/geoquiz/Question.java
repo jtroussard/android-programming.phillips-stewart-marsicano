d5bc4907ad5f9df1d4bd1d2fa2939df0966ab53f
Jacques Troussard <jacques.troussard@gmail.com>
2017-12-21 14:36:58 -0500

    chapter three project one page 74
    fix previous button (looping)
    wip register answered or not

--- GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/Question.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/Question.java
@@ -8,10 +8,20 @@ public class Question {
 
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mAnswered;
 
     public Question (int textResId, boolean answerTrue) {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
+        mAnswered = false;
+    }
+
+    public boolean getAnswered() {
+        return this.mAnswered;
+    }
+
+    public void setAnswered(boolean v) {
+        mAnswered = v;
     }
 
     public int getTextResId() {
--- GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -2,18 +2,21 @@ package com.bignerdranch.andriod.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPrevButton;
+    private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -30,7 +33,13 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        // Retrieve data from saved state if present
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         // Text view
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -45,7 +54,7 @@ public class QuizActivity extends AppCompatActivity {
         });
 
         // Next button
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,37 +64,84 @@ public class QuizActivity extends AppCompatActivity {
         });
 
         // Prev button
-        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton = (Button) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mCurrentIndex > 0) {
                     mCurrentIndex = (mCurrentIndex - 1);
+                } else {
+                    mCurrentIndex = mQuestionBank.length - 1;
                 }
                 updateQuestion();
             }
         });
-
         updateQuestion();
 
-        // True button
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(true);
-            }
-        });
+        // True & False buttons
+        if (mQuestionBank[mCurrentIndex].getAnswered()) {
+            Log.d(TAG, "Already Answered");
+            mTrueButton.setEnabled(false);
+            mFalseButton.setEnabled(false);
+        } else {
+            Log.d(TAG, "Answering Question");
+            mTrueButton = (Button) findViewById(R.id.true_button);
+            mTrueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkAnswer(true);
+                    registerAnswer();
+                }
+            });
+            mFalseButton = (Button) findViewById(R.id.false_button);
+            mFalseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkAnswer(false);
+                    registerAnswer();
+                }
+            });
+        }
+        Log.d(TAG, "value of answered is ".concat(String.valueOf(mQuestionBank[mCurrentIndex].getAnswered())));
 
-        // False button
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(false);
-            }
-        });
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState() called");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestory() called");
     }
 
     private void updateQuestion() {
@@ -105,5 +161,9 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void registerAnswer() {
+        mQuestionBank[mCurrentIndex].setAnswered(true);
     }
 }

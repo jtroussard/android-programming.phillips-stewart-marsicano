87d1a16fc00e8c02ad55ad44a5b513ebb9e04f49
Jacques Troussard <jacques.troussard@gmail.com>
2017-12-21 12:11:39 -0500

    clean up

--- GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/andriod/geoquiz/QuizActivity.java
@@ -2,14 +2,11 @@ package com.bignerdranch.andriod.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
 
